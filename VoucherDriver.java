7c319f8426e55b5c2c07ec33c33b710ff7f6fb21
Jessicaaaaaaaaaaaaaaaaaaaa <75866391+Jessicaaaaaaaaaaaaaaaaaaaa@users.noreply.github.com>
2021-01-28 14:17:22 -0600

    Add files via upload

--- VoucherDriver.java
+++ VoucherDriver.java
@@ -8,48 +8,67 @@ public class VoucherDriver {
 		boolean check = true;
 		Voucher.initialize(1001, 2001, 7);
 		
-		System.out.println("XYZ COMPANY Voucher Manager\n");
+		String purchaseDate = "";
+		double amount = 0;
+		String debitAccount = "";
+		String vendor = "";
 		
-		System.out.println("Activities available:\n" 
-				+ "1. create voucher\n"
-				+ "2. print voucher checks\n" + "3. issue checks\n\n" 
-				+ "Enter number of choice (or zero to quit): ");
+		System.out.println("XYZ COMPANY Voucher Manager");
 		
 		while (check) {
+			
+			System.out.println("\nActivities available:\n" 
+				+ "1. create voucher\n"
+				+ "2. print voucher checks\n" 
+				+ "3. issue checks\n\n" 
+				+ "Enter number of choice (or zero to quit): ");
 			
 			String activityChoice =  scan.nextLine();
 			
 			if(activityChoice.equals("1")) {
 				
-				// ask for info using scanner
-				// Voucher.addVoucher();
-			}
-			
-			if(activityChoice.equals("2")) {
-				Voucher.printData();
+				System.out.print("Enter purchase date (dd/mm/yyyy): ");
+				purchaseDate = scan.nextLine();
 				
+				System.out.print("Enter amount: $ ");
+				amount = Double.parseDouble(scan.nextLine());
+				
+				System.out.print("Enter name of account to debit: ");
+				debitAccount = scan.nextLine();
+				
+				System.out.print("Enter name of vendor: ");
+				vendor = scan.nextLine();
+				
+				Voucher.addVoucher(purchaseDate, amount, debitAccount, vendor);
+				
+				}
+			
+			else if(activityChoice.equals("2")) {
+				Voucher.printData();	
 			}
 			
-			if(activityChoice.equals("3")) {
-				// Voucher find (get voucher number)
-				// no vouchers have that number verifier
-				// get purchase date
-				// Voucher.issueCheck();
+			else if(activityChoice.equals("3")) {
+				int voucherNumber = 1002;
+				boolean b = Voucher.find(voucherNumber);
+				
+				if (b) {
+					
+					Voucher.issueCheck(voucherNumber, purchaseDate);
+				}
+				
+				else {
+	
+					System.out.println("No vouchers have that number.");
+				}
 				
 			}
 		
-			if(activityChoice.equals("0")) {
+			else if(activityChoice.equals("0")) {
 				break;
 			}
 		
-			if(!(activityChoice.equals("0") || activityChoice.equals("1") || 
-					activityChoice.equals("2") || activityChoice.equals("3"))) {
-				System.out.println("invalid choice. Try again. \n");
-				
-				System.out.println("Activities available:\n" 
-						+ "1. create voucher\n"
-						+ "2. print voucher checks\n" + "3. issue checks\n\n" 
-						+ "Enter number of choice (or zero to quit): ");
+			else {
+				System.out.println("invalid choice. Try again.");	
 			}
 		}
 	
